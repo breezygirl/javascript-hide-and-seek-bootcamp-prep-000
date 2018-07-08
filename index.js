1c3dbc7ef30c933f8f3088b7d4e0fe4c5d0ecbdc
Beerscheba C <designergo@aol.com>
2018-07-08 23:15:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ return document.querySelector(selector);
 function nestedTarget(){
 return document.querySelector('#nested .target');
 }
-
+function increaseRankBy(n) {
+	var lis = document
+		.getElementById('app')
+		.querySelectorAll('ul.ranked-list li');
+	for (let i = 0; i < lis.length; i++) {
+		lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+	}
+}
