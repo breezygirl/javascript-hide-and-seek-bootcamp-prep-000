64767b873ff4716c55bcb4271e7c23ea40b9ca8a
Beerscheba C <designergo@aol.com>
2018-07-08 23:07:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function getFirstSelector(selector){
 return document.querySelector(selector);
   }
 
+
+function nestedTarget(){
+return document.querySelector('#nested.target');
+}
+
+increaseRankBy(n){
+  
+  
+}
